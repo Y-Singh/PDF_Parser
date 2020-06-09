0fa31c040d437f9ea6312989a3d6ff980df6561a
Y-Singh <=>
2020-06-08 21:00:07 -0400

    Finished parsing for 702-n

--- src/tester.py
+++
@@ -1,9 +0,0 @@
-import pdfminer.high_level
-from . import __path__
-print(__path__)
-t = pdfminer.high_level.extract_text('PackSlips\\209721.pdf')
-t=t.split('\n')
-n = list()
-for i in t:
-    n.append(i + '<br>')
-print(''.join(n))
